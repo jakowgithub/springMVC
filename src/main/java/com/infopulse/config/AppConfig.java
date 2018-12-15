95745518b70f00d62fe08806e552a14e298cf4d7
galkin <oleksandr.galkin@intapp.com>
2018-12-15 11:11:55 +0200

    add configuration

---
+++ src/main/java/com/infopulse/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.infopulse.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.infopulse")
+public class AppConfig {
+}
